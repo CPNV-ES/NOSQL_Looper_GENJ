7a77974a0d48a9cca9c95d2497f6d81bf24dd25a
Ethann Schneider <ethann@shkermit.ch>
2024-08-29 10:45:40 +0200

    Removed link and script bootstrap

--- src/views/template.php
+++ src/views/template.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
-    <link href="/Assets/bootstrap-5.3.3/css/bootstrap.min.css" rel="stylesheet">
-    <script src="/Assets/bootstrap-5.3.3/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <?php echo $content ?>
