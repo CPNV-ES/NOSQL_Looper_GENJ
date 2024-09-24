29374932fc8aadb5e61ee4ec1f90a1ed00e0c198
Ethann Schneider <ethann@shkermit.ch>
2024-09-24 21:50:26 +0200

    chore: change navigation to fit the new error code

--- src/controllers/navigation.php
+++ src/controllers/navigation.php
@@ -7,10 +7,20 @@ function home()
 
 function create_an_excercices()
 {
-	include VIEW_DIR . '/create-an-excercice.php';
+	include VIEW_DIR . '/create_an_excercice.php';
 }
 
 function lost()
 {
-	include VIEW_DIR . '/lost.php';
+	include VIEW_DIR . '/errors/lost.php';
+}
+
+function bad_request()
+{
+	include VIEW_DIR . '/errors/bad_request.php';
+}
+
+function method_not_allowed()
+{
+	include VIEW_DIR . '/errors/method_not_allowed.php';
 }
