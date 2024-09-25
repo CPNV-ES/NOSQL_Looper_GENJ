ddbe47213f2792505779bda83e2284d1bde2527c
Guillaume Aubert <guillaume@aubert.ws>
2024-09-25 11:48:40 +0200

    fix: implementing template

--- src/views/exercices_root.php
+++ src/views/exercices_root.php
@@ -1,41 +1,46 @@
+<?php
+$title = 'ExerciseLooper';
+
+ob_start();
+?>
+
+<link rel="stylesheet" href="/assets/css/exercises_root.css">
+
 <header class="heading answering">
-  <section class="container">
-    <a href="/"><img src="/assets/img/logo.png" /></a>
-  </section>
+	<section class="container">
+		<a href="/"><img src="/assets/img/logo.png" /></a>
+	</section>
 </header>
 
 <main class="container">
- 	<!DOCTYPE html>
-<html>
-	<head>
-		<title>ExerciseLooper</title>
-		<link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
-		<link rel="stylesheet" href="/assets/css/exercises_root.css">
-	</head>
-	<body>
-		<?php
-			$exercises = [
-				[
-					'id' => 1,
-					'name' => 'Anglais'
-				],
-				[
-					'id' => 2,
-					'name' => 'Francais'
-				]
+	<?php
+		$exercises = [
+			[
+				'id' => 1,
+				'name' => 'Anglais'
+			],
+			[
+				'id' => 2,
+				'name' => 'Francais'
 			]
-		?>
-		<ul class="answering-list">
-			<?php foreach ($exercises as $exercise): ?>
-				<li class="row">
-				<div class="column card">
-					<div class="title"><?= htmlspecialchars($exercise['name']); ?></div>
-					<a class="button" href="/exercises/<?= $exercise['id']; ?>/fulfillments/new">Take it</a>
+		]
+	?>
+	<ul class="answering-list">
+		<?php foreach ($exercises as $exercise): ?>
+		<li class="row">
+			<div class="column card">
+				<div class="title">
+					<?= htmlspecialchars($exercise['name']); ?>
 				</div>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-	</body>
-</html>
+				<a class="button"
+					href="/exercises/<?= $exercise['id']; ?>/fulfillments/new">Take
+					it</a>
+			</div>
+		</li>
+		<?php endforeach; ?>
+	</ul>
 </main>
+<?php
+$content = ob_get_clean();
+require VIEW_DIR . '/template.php';
+?>
