caa9949f4999b083db19fa0ed14aff4c8e719459
Guillaume Aubert <guillaume@aubert.ws>
2024-10-08 11:54:03 +0200

    feat: Add new exercise status management functions to controller

--- src/controllers/exercise_controller.php
+++ src/controllers/exercise_controller.php
@@ -12,3 +12,18 @@ function createExercise()
 	$exercise = Exercises::create($_POST['exercise_title']);
 	header('Location: /exercises/' . $exercise->getId() . '/fields');
 }
+
+function setExerciseAsAnswering(int $id)
+{
+	$exercise = new Exercises($id);
+	if ($exercise->getStatus() === Status::Building)
+		$exercise->setExerciseAs(Status::Answering);
+	header('Location: /exercises');
+}
+function setExerciseAsClosed(int $id)
+{
+	$exercise = new Exercises($id);
+	if ($exercise->getStatus() === Status::Answering)
+		$exercise->setExerciseAs(Status::Closed);
+	header('Location: /exercises');
+}
