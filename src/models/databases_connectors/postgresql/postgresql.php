ba6f07dd56c6abad79b6dbcf7b989cb77195f70b
Jomana Kämpf <pp37ufi@eduvaud.ch>
2024-10-09 11:41:28 +0200

    fix: fixed modify methode of postgresql class

--- src/models/databases_connectors/postgresql/postgresql.php
+++ src/models/databases_connectors/postgresql/postgresql.php
@@ -26,19 +26,14 @@ class Postgresql
 	{
 		if ($args) {
 			$statement = $this->db->prepare($squery);
-			$statement->bindParam(':id', $args[':id']);
 			foreach ($args as $key => $value) {
-				if (is_int($value)) {
-					$statement->bindParam(':' . $key, $value);
-				} else {
-					$statement->bindParam(':' . $key, $value);
-				}
+				$statement->bindParam( $key, $value);
 			}
 			$statement->execute();
 		} else {
 			$this->db->exec($squery);
 		}
-
+		
 		return;
 	}
 }
